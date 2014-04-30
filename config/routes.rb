7020379a7fd2da5d88a8e448f33cee1923452b46
acrewson <acrewson@gmail.com>
2014-04-30 07:29:30 -0500

    Layout tweak

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Expense::Application.routes.draw do
   root :to => 'welcome#main'
 
   devise_for :users
+  # Helpful Devise links
+    # On controllers: http://stackoverflow.com/questions/6234045/how-do-you-access-devise-controllers
+
+
+
+
 
   # Resources
   resources :logs, shallow: true do
