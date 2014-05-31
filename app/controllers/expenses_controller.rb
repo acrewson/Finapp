6163bbba2347a620e2602574a2d0822b9bec0457
acrewson <acrewson@gmail.com>
2014-05-30 23:36:43 -0500

    Updated expense summary

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,6 +4,8 @@ class ExpensesController < ApplicationController
   # This shows the expenses on the log
   def index
     @expenses = Log.find_by(:id => params[:log_id]).expenses.order(:date_cash)
+    @dates = @expenses.select("date_cash, sum(amount) as date_amount").group("date_cash").order(:date_cash)
+
   end
 
   # For creating a new expense on the log
