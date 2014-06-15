23aa58c2b3ac7242ec93f5d5afafee5194494edd
acrewson <acrewson@gmail.com>
2014-06-14 19:47:58 -0500

    Customized sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Finapp::Application.routes.draw do
 
   root :to => 'welcome#main'
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   # Helpful Devise links
     # On controllers: http://stackoverflow.com/questions/6234045/how-do-you-access-devise-controllers
+    # On customizing: http://www.jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html
 
 
 
