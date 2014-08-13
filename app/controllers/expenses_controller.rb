4d4ba96897cf88d123f880868bf3b9037d934b98
acrewson <acrewson@gmail.com>
2014-08-12 22:36:14 -0500

    8/12 #5

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,9 +4,9 @@ class ExpensesController < ApplicationController
   # This shows the expenses on the log
   def index
     if params[:date] == "cash"
-      @expenses = Log.find_by(:id => params[:log_id]).expenses.order(:date_cash).select("id, log_id, description, amount, date(date_cash) as date_show, category_id, deprec_months, method_id, user_id, is_outlier")
+      @expenses = Log.find_by(:id => params[:log_id]).expenses.order(:date_cash).select("id, log_id, description, amount, date(date_cash) as ds, category_id, deprec_months, method_id, user_id, is_outlier")
 
-      @dates = Log.find_by(:id => params[:log_id]).expenses.select("date(date_cash) as 'date_show', sum(amount) as date_amount").group("date_show").order("date_show")
+      @dates = Log.find_by(:id => params[:log_id]).expenses.select("date(date_cash) as ds, sum(amount) as date_amount").group("ds").order("ds")
 
     else
       @expenses = Log.find_by(:id => params[:log_id]).expenses.order(:date_expense).select("id, log_id, description, amount, date(date_expense) as date_show, category_id, deprec_months, method_id, user_id, is_outlier")
